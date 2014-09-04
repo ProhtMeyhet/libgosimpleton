02ad3ea4caaccc658e9e07a09d82b9e96883c344
ProhtMeyhet <sebastian@ifyouwantblood.de>
2014-09-05 01:16:04 +0200

    rethink

--- simplee/simplee.go
+++ simplee/simplee.go
@@ -4,34 +4,86 @@ import(
 	"fmt"
 )
 
-type ErrorF struct {
+// E represents an error
+type E struct {
 	format, formatted string
 }
 
-func New(to string) *ErrorF {
-	return &ErrorF{ format: to }
-}
-
-func (errorF *ErrorF) Format(what ...interface{}) *ErrorF {
-	if len(what) > 0 {
-		errorF.formatted = fmt.Sprintf(errorF.format, what...)
+// create a new error with string format
+func New(to string) *E {
+	if to == "" {
+		panic("cannot initialise empty error!")
 	}
 
-	return errorF
+	return &E{ format: to }
 }
 
-func (errorF *ErrorF) Error() string {
-	if errorF.formatted != "" {
-		return errorF.formatted
-	}
-
-	return errorF.format
+// returns a copy of this *E
+func (e *E) Copy() *E {
+	eNew := New(e.format)
+	eNew.formatted = e.formatted
+	return eNew
 }
 
-func (errorF *ErrorF) IsEqual(to error) bool {
-	if ef, ok := to.(*ErrorF); ok {
-		return errorF.format == ef.format
+// returns format
+func (e *E) GetFormat() string {
+	return e.format
+}
+
+// formats string
+func (e *E) GetFormatted(what ...interface{}) string {
+	return fmt.Sprintf(e.format, what...)
+}
+
+// format error and returns a copy of that error
+func (e *E) Format(what ...interface{}) *E {
+	eCopy := e.Copy()
+	eCopy.formatted = eCopy.GetFormatted(what...)
+	return eCopy
+}
+
+// interface function, return formatted if possible
+// otherwise returns format
+func (e *E) Error() string {
+	if e.formatted != "" {
+		return e.formatted
 	}
 
-	return errorF.format == to.Error()
+	return e.format
+}
+
+// tests if format is equal
+// if given error is no *E, checks if format is equal to
+// interface Error() output
+func (e *E) IsEqual(to error) bool {
+	if to == nil {
+		return false
+	}
+
+	if ef, ok := to.(*E); ok {
+		return e.format == ef.format
+	}
+
+	return e.format == to.Error()
+}
+
+// tests if format is equal and formatted is equal
+// if not formatted, they are considered equal now
+// if non *E given, tests if formatted equal to interface
+// Error() output
+func (e *E) IsSame(to error) bool {
+	if to == nil {
+		return false
+	}
+
+	// if both havn't been formatted, they are the same
+	if ef, ok := to.(*E); ok {
+		if e.format == ef.format {
+			return e.formatted == ef.formatted
+		} else {
+			return false
+		}
+	}
+
+	return e.formatted == to.Error()
 }
--- simplee/simplee_test.go
+++ simplee/simplee_test.go
@@ -6,8 +6,9 @@ import(
 	"testing"
 )
 
+var p1, p2, p3 = "Apple is %s", "crap", "still crap"
+
 func TestSimplee(t *testing.T) {
-	p1, p2 := "Apple is %s", "crap"
 	expected := fmt.Sprintf(p1, p2)
 	e := New(p1)
 
@@ -30,8 +31,37 @@ func TestSimplee(t *testing.T) {
 	if e5 := errors.New(p2); e.IsEqual(e5) {
 		t.Errorf("unexpected: e == e5")
 	}
+
+	if e.IsEqual(nil) {
+		t.Errorf("unexpected: e == nil")
+	}
+
+	if !e.IsEqual(doError2(e)) {
+		t.Errorf("unexpected: e != doError2( e )!")
+	}
+
+	//IsSame()
+	if e.IsSame(nil) {
+		t.Errorf("unexpected: e.IsSame( nil )")
+	}
+
+	if !IsEqual(doError(e), doError2(e)) {
+		t.Errorf("unexpected: doError( e ) != doError2( e )!")
+	}
+
+	if IsSame(doError(e), doError2(e)) {
+		t.Errorf("unexpected: doError( e ) IsSame doError2( e ) )!")
+	}
+
+	if e.IsSame(doError2(e)) {
+		t.Errorf("unexpected: e IsSame doError2( e )!")
+	}
 }
 
-func doError(e *ErrorF) error {
-	return e.Format("crap")
+func doError(e *E) error {
+	return e.Format(p2)
+}
+
+func doError2(e *E) error {
+	return e.Format(p3)
 }
